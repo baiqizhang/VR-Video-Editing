8e94b28505fecb84e41d98aaf4939b8e56908639
baiqizhang <baiqiz@andrew.cmu.edu>
2016-06-23 21:14:41 -0700

    add logo to bottom

--- polar.py
+++ polar.py
@@ -1,10 +1,24 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('data/circle.png')
+logo = cv2.imread('data/circle.png')
 
-img2 = cv2.linearPolar(img,(128,128),128,flags=cv2.INTER_LINEAR+cv2.WARP_FILL_OUTLIERS)
+warped = cv2.linearPolar(logo,(128,128),120,flags=cv2.INTER_LINEAR+cv2.WARP_FILL_OUTLIERS)
 
-cv2.imshow("img1",img)
-cv2.imshow("img2",img2)
+# grab the dimensions of the image and calculate the center
+# of the image
+(h, w) = warped.shape[:2]
+center = (w / 2, h / 2)
+
+# rotate the image by 180 degrees
+M = cv2.getRotationMatrix2D(center, 90, 1.0)
+rotated = cv2.warpAffine(warped, M, (w, h))
+
+
+# perform the actual resizing of the image and show it
+dim = (1280,320)
+resized = cv2.resize(rotated, dim, interpolation = cv2.INTER_AREA)
+
+cv2.imshow("logo",logo)
+cv2.imshow("rotated",resized)
 cv2.waitKey(0)
--- trim.py
+++ trim.py
@@ -1,29 +1,51 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture('bird.mp4')
+visualize = True#False
 
+logo = cv2.imread('data/circle.png')
+
+warped = cv2.linearPolar(logo,(128,128),120,flags=cv2.INTER_LINEAR+cv2.WARP_FILL_OUTLIERS)
+
+# grab the dimensions of the image and calculate the center
+# of the image
+(h, w) = warped.shape[:2]
+center = (w / 2, h / 2)
+
+# rotate the image by 180 degrees
+M = cv2.getRotationMatrix2D(center, 90, 1.0)
+rotated = cv2.warpAffine(warped, M, (w, h))
+
+
+# perform the actual resizing of the image and show it
+dim = (1280,120)
+resized = cv2.resize(rotated, dim, interpolation = cv2.INTER_AREA)
+
+
+
+cap = cv2.VideoCapture('data/bird.mp4')
 
 if cap.isOpened():
     ret, frame = cap.read()
     rows, cols = frame.shape[:2]
-#    cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
+    print rows,cols
 
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter('output.mov',fourcc, 15.0, (cols,rows))
+    out = cv2.VideoWriter('data/output.mov',fourcc, 15.0, (cols,rows))
+
+    if visualize:
+        cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
 
 while(cap.isOpened()):
     ret, frame = cap.read()
 
-#    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    frame[(4*rows/5):rows,:]=0
-    
+    frame[rows-120:rows,:] = resized
     out.write(frame)
-    
-#    cv2.imshow('frame',frame)
-#    if cv2.waitKey(1) & 0xFF == ord('q'):
-#        break
+
+    if visualize:
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 cap.release()
 out.release()
